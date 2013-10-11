a5d85ff4be09287584228659d54fc43339437b09
Jan-Christian Foeh <jan@programmanstalt.de>
2013-10-11 13:30:05 +0200

    Basic project setup

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  var attache = {
+    buildDir: 'dist'
+  };
+
+  // Project configuration.
+  grunt.initConfig({
+    appConfig: attache,
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['<%= appConfig.buildDir %>'],
+    // copy: {
+    //   // NOTE: copy MUST have a target - just using copy: { files: ... } yields an 'missing indexOf' error
+    //   build: {
+    //     files: [
+    //       {src: ['css/fonts/*'], dest: '<%= appConfig.buildDir %>/', filter: 'isFile'},
+    //       {src: ['videos/*'], dest: '<%= appConfig.buildDir %>/', filter: 'isFile'}
+    //     ]
+    //   }
+    // },
+    // uglify: {
+    //   options: {
+    //     report: 'gzip',
+    //     mangle: false
+    //   }
+    // }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // grunt.registerTask('build', [
+  //   'clean',
+  //   'uglify'
+  // ]);
+};
